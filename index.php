578f40231b944d82510cae09248d863b0593f19e
pavlos <p.pp256@yahoo.com>
2018-06-23 04:35:47 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    
+    <head>
+        <title> 777 Slot Machine </title>
+        <style type="text/css">
+            @import url("css/styles.css")
+        </style>
+    </head>
+    
+    <body>
+        
+        <?php
+        include 'inc/functions.php';
+        ?>
+        
+        <div id="main">
+            <form>
+                <input type="submit" value="Spin!" />
+            </form>
+            <?php play(); ?>
+        </div>
+        
+    </body>
+    
+</html>
